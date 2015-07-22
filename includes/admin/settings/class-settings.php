28b8a3bcfe818affdbc58971615da31265312ea5
Frankie Jarrett <frankie@x-team.com>
2015-07-22 14:17:57 -0500

    Properly encapsulate integrations loader class, add useful helper methods

--- includes/admin/settings/class-settings.php
+++ includes/admin/settings/class-settings.php
@@ -336,7 +336,7 @@ class Affiliate_WP_Settings {
 						'name' => __( 'Integrations', 'affiliate-wp' ),
 						'desc' => sprintf( __( 'Choose the integrations to enable. If you are not using any of these, you may use the <strong>[affiliate_conversion_script]</strong> short code to track and create referrals. Refer to the <a href="%s" target="_blank">documentation</a> for help using this.', 'affiliate-wp' ), 'http://affiliatewp.com/docs/custom-referral-tracking/' ),
 						'type' => 'multicheck',
-						'options' => affiliate_wp()->integrations->get_integrations()
+						'options' => affiliate_wp()->integrations->get_registered()
 					)
 				)
 			),
--- includes/class-integrations.php
+++ includes/class-integrations.php
@@ -2,56 +2,121 @@
 
 class Affiliate_WP_Integrations {
 
+	/**
+	 * An array of all registered integrations
+	 *
+	 * @var array
+	 */
+	private $registered = array();
+
+	/**
+	 * An array of the enabled integrations
+	 *
+	 * @var array
+	 */
+	private $enabled = array();
+
+	/**
+	 * Class constructor
+	 */
 	public function __construct() {
+
+		$this->registered = (array) apply_filters( 'affwp_integrations',
+			array(
+				'edd'            => 'Easy Digital Downloads',
+				'formidablepro'  => 'Formidable Pro',
+				'gravityforms'   => 'Gravity Forms',
+				'exchange'       => 'iThemes Exchange',
+				'jigoshop'       => 'Jigoshop',
+				'marketpress'    => 'MarketPress',
+				'membermouse'    => 'MemberMouse',
+				'memberpress'    => 'MemberPress',
+				'ninja-forms'    => 'Ninja Forms',
+				'pmp'            => 'Paid Memberships Pro',
+				'rcp'            => 'Restrict Content Pro',
+				'shopp'	         => 'Shopp',
+				'sproutinvoices' => 'Sprout Invoices',
+				'woocommerce'    => 'WooCommerce',
+				'wpec'           => 'WP e-Commerce',
+			)
+		);
+
+		$this->enabled = (array) apply_filters( 'affwp_enabled_integrations', affiliate_wp()->settings->get( 'integrations', array() ) );
 
 		$this->load();
 
 	}
 
-	public function get_integrations() {
+	/**
+	 * Load each enabled integration
+	 */
+	private function load() {
 
-		return apply_filters( 'affwp_integrations', array(
-			'edd'            => 'Easy Digital Downloads',
-			'formidablepro'  => 'Formidable Pro',
-			'gravityforms'   => 'Gravity Forms',
-			'exchange'       => 'iThemes Exchange',
-			'jigoshop'       => 'Jigoshop',
-			'marketpress'    => 'MarketPress',
-			'membermouse'    => 'MemberMouse',
-			'memberpress'    => 'MemberPress',
-			'ninja-forms'    => 'Ninja Forms',
-			'pmp'            => 'Paid Memberships Pro',
-			'rcp'            => 'Restrict Content Pro',
-			'shopp'	         => 'Shopp',
-			'sproutinvoices' => 'Sprout Invoices',
-			'woocommerce'    => 'WooCommerce',
-			'wpec'           => 'WP e-Commerce',
-		) );
+		if ( ! $this->enabled ) {
+			return;
+		}
 
-	}
-
-	public function get_enabled_integrations() {
-		return affiliate_wp()->settings->get( 'integrations', array() );
-	}
-
-	public function load() {
-
-		// Load each enabled integrations
 		require_once AFFILIATEWP_PLUGIN_DIR . 'includes/integrations/class-base.php';
-
-		$enabled = apply_filters( 'affwp_enabled_integrations', $this->get_enabled_integrations() );
 
 		do_action( 'affwp_integrations_load' );
 
-		foreach( $enabled as $filename => $integration ) {
+		foreach ( $this->enabled as $filename => $label ) {
 
-			if( file_exists( AFFILIATEWP_PLUGIN_DIR . 'includes/integrations/class-' . $filename . '.php' ) ) {
+			if ( is_readable( AFFILIATEWP_PLUGIN_DIR . 'includes/integrations/class-' . $filename . '.php' ) ) {
+
 				require_once AFFILIATEWP_PLUGIN_DIR . 'includes/integrations/class-' . $filename . '.php';
+
 			}
 
 		}
 
 		do_action( 'affwp_integrations_loaded' );
+
+	}
+
+	/**
+	 * Return an array of all registered integrations
+	 *
+	 * @return array
+	 */
+	public function get_registered() {
+
+		return (array) $this->registered;
+
+	}
+
+	/**
+	 * Return an array of all enabled integrations
+	 *
+	 * @return array
+	 */
+	public function get_enabled() {
+
+		return (array) $this->enabled;
+
+	}
+
+	/**
+	 * Check to see if a particlar integration has been registered
+	 *
+	 * @param  string $integration
+	 * @return bool
+	 */
+	public function is_registered( $integration ) {
+
+		return array_key_exists( $integration, $this->get_registered() );
+
+	}
+
+	/**
+	 * Check to see if a particlar integration is enabled
+	 *
+	 * @param  string $integration
+	 * @return bool
+	 */
+	public function is_enabled( $integration ) {
+
+		return array_key_exists( $integration, $this->get_enabled() );
 
 	}
 
